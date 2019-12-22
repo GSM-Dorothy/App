30d9965761055c6e46356732ace70ef46acffc10
Unperknown <wnsdud105@gmail.com>
2019-12-22 16:55:12 +0900

    fixed meals api

--- server/api/school/index.js
+++ server/api/school/index.js
@@ -2,6 +2,7 @@ const Router = require('koa-router')
 const school = new Router()
 const schoolCtrl = require('./school.controller')
 
+school.get('/meal/:year/:month', schoolCtrl.getMeal)
 school.get('/meal/:year/:month/:day', schoolCtrl.getMeal)
 school.get('/schedule/:year/:month/:day', schoolCtrl.getSchedule)
 
--- server/api/school/school.controller.js
+++ server/api/school/school.controller.js
@@ -5,12 +5,40 @@ const RemainAdministrator = require('models/remain_administrator')
 
 School.init(SchoolAPI.Type.HIGH, SchoolAPI.Region.GWANGJU, 'F100000120')
 
-exports.getMeal = async (ctx) => {
-  let year = ctx.params.year
-  let month = ctx.params.month
-  const meal = (await School.getMeal(year, month))[ctx.params.day]
+let jsonifyMeal = meal => {
+  let menus = meal.replace(/\n/g, ',').replace(/[1234567890*.]/gi, '').split(',')
 
-  ctx.body = meal // TO-DO: JSON 반환 후 ctx.body에 할당
+  let breakfastIndex = menus.indexOf('[조식]') === -1 ? menus.length : menus.indexOf('[조식]')
+  let lunchIndex = menus.indexOf('[중식]') === -1 ? menus.length : menus.indexOf('[중식]')
+  let dinnerIndex = menus.indexOf('[석식]') === -1 ? menus.length : menus.indexOf('[석식]')
+
+  return {
+    조식: menus.slice(breakfastIndex + 1, lunchIndex),
+    중식: menus.slice(lunchIndex + 1, dinnerIndex),
+    석식: menus.slice(dinnerIndex + 1)
+  }
+}
+exports.getMeal = async (ctx) => {
+  let meals = await School.getMeal(ctx.params.year, ctx.params.month)
+
+  delete meals.year
+  delete meals.month
+  delete meals.day
+  delete meals.today
+
+  if (ctx.params.day) {
+    let mealJSON = {}
+
+    mealJSON[ctx.params.day] = meals[ctx.params.day]
+
+    meals = mealJSON
+  }
+
+  for (let i in meals) {
+    meals[i] = jsonifyMeal(meals[i])
+  }
+
+  ctx.body = meals
 }
 
 exports.getSchedule = async (ctx) => {
