b09a190008dcba48b9878507474502c356300a0e
Hana <momenthana@kakao.com>
2019-12-28 19:32:00 +0900

    급식 날짜 선택 추가

--- src/views/meals.vue
+++ src/views/meals.vue
@@ -1,7 +1,21 @@
 <template>
 <v-content>
   <v-container fluid>
-    <v-row dense>
+    <v-sheet>
+      <v-toolbar flat>
+        <v-btn outlined class="mr-4" @click="setToday">
+          Today
+        </v-btn>
+        <v-btn fab text small @click="prev">
+          <v-icon small>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-toolbar-title>{{ title }}</v-toolbar-title>
+        <v-btn fab text small @click="next">
+          <v-icon small>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-sheet>
+    <v-row>
       <v-col v-for="title in Object.keys(posts)" :key="title" :cols="4">
         <v-card class="elevation-12 ma-3">
           <v-card-text>
@@ -23,15 +37,43 @@
 import axios from 'axios'
 
 export default {
-  data () {
-    return {
-      posts: []
-    }
-  },
+  data: () => ({
+    posts: [],
+    day: new Date(),
+    title: ''
+  }),
   created () {
-    axios.get('http://api.dorothy.gsmhs.kr/school/meal/2019/12/27').then((response) => {
+    this.title = this.day.getFullYear() + '년 ' + (this.day.getMonth() + 1) + '월 ' + this.day.getDate() + '일'
+    var get = 'http://api.dorothy.gsmhs.kr/school/meal/' + this.day.getFullYear() + '/' + (this.day.getMonth() + 1) + '/' + this.day.getDate()
+    axios.get(get).then((response) => {
       this.posts = response.data
     })
+  },
+  methods: {
+    setToday () {
+      this.day = new Date()
+      this.title = this.day.getFullYear() + '년 ' + (this.day.getMonth() + 1) + '월 ' + this.day.getDate() + '일'
+      var get = 'http://api.dorothy.gsmhs.kr/school/meal/' + this.day.getFullYear() + '/' + (this.day.getMonth() + 1) + '/' + this.day.getDate()
+      axios.get(get).then((response) => {
+        this.posts = response.data
+      })
+    },
+    prev () {
+      this.day.setDate(this.day.getDate() - 1)
+      this.title = this.day.getFullYear() + '년 ' + (this.day.getMonth() + 1) + '월 ' + this.day.getDate() + '일'
+      var get = 'http://api.dorothy.gsmhs.kr/school/meal/' + this.day.getFullYear() + '/' + (this.day.getMonth() + 1) + '/' + this.day.getDate()
+      axios.get(get).then((response) => {
+        this.posts = response.data
+      })
+    },
+    next () {
+      this.day.setDate(this.day.getDate() + 1)
+      this.title = this.day.getFullYear() + '년 ' + (this.day.getMonth() + 1) + '월 ' + this.day.getDate() + '일'
+      var get = 'http://api.dorothy.gsmhs.kr/school/meal/' + this.day.getFullYear() + '/' + (this.day.getMonth() + 1) + '/' + this.day.getDate()
+      axios.get(get).then((response) => {
+        this.posts = response.data
+      })
+    }
   }
 }
 </script>
--- src/views/remain.vue
+++ src/views/remain.vue
@@ -1,16 +1,79 @@
 <template>
 <v-content>
   <v-container fluid>
-    <v-flex mb-4>
-      <h1 class="display-2 font-weight-bold mb-3">
-        Remain
-      </h1>
-      <p class="subheading font-weight-regular">
-        For help and collaboration with other Vuetify developers,
-        <br>please join our online
-        <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-      </p>
-    </v-flex>
+    <h1 class="display-2 font-weight-bold mb-3">
+      잔류
+    </h1>
+    <v-row>
+      <v-col cols=6>
+        <v-card class="elevation-12 ma-3">
+          <v-card-text>
+            <p class="text--primary">
+              현재 담당선생님
+            </p>
+            <p class="display-1 text--primary">
+              장재원 선생님
+            </p>
+            <p class="text--primary">
+              010-1234-5678
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols=6>
+        <v-card class="elevation-12 ma-3">
+          <v-card-text>
+            <p class="text--primary">
+              다음 담당선생님
+            </p>
+            <p class="display-1 text--primary">
+              사감 선생님
+            </p>
+            <p class="text--primary">
+              010-1234-5678
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols=3>
+        <v-card class="elevation-12 ma-3" height="150">
+          <v-card-text>
+            <p class="display-1 text--primary">
+              호실
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols=3>
+        <v-card class="elevation-12 ma-3" height="150">
+          <v-card-text>
+            <p class="display-1 text--primary">
+              학습
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols=3>
+        <v-card class="elevation-12 ma-3" height="150">
+          <v-card-text>
+            <p class="display-1 text--primary">
+              외출
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols=3>
+        <v-card class="elevation-12 ma-3" height="150">
+          <v-card-text>
+            <p class="display-1 text--primary">
+              외박
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </v-content>
 </template>
